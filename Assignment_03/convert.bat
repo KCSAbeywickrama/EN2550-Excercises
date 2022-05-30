e9c3150eb5b941a7c68f0ab0b0118c6e504bd260
CSA <abeywickramakcs.19@uom.lk>
2022-05-30 21:27:28 +0530

    submit

--- Assignment_03/convert.bat
+++ Assignment_03/convert.bat
@@ -1,2 +1,2 @@
-ffmpeg -i conveyor_result_190018V.mp4 -c:v libx264 -qp 0 conveyor_result_comp_190018V
+ffmpeg -i conveyor_result_190018V.mp4 -c:v libx264 -qp 0 conveyor_result_comp_190018V.mp4
 pause
